cb65e2369034a6a22957d4d50a289d3639ef1f01
BenReichwein <ben.basque@icloud.com>
2021-03-21 01:26:42 -0400

    Nav bar basics have been added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Landing from './pages/Landing.js'
+import Nav from './Nav'
+import './styles/App.scss'
 
 function App() {
   return (
       <div>
+          <Nav />
           <Landing />
       </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './styles/Nav.scss'
+
+class Nav extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked: false,
+        }
+    }
+
+    // Handles click
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+    render() {
+        return(
+            <nav className="NavbarItems">
+                <a href={"/"} style={{textDecoration : "none"}}><h1 className="navbar-logo">SAFETY NET</h1></a>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}/>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    <li>
+                        <a className="nav-links" href='/#'>
+                            How it works
+                        </a>
+                    </li>
+                    <li>
+                        <a className="nav-links" href='/#'>
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a className="nav-links" href='/#'>
+                            Contact
+                        </a>
+                    </li>
+                    <li>
+                        <a className="nav-links" href='/#'>
+                            Download
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Nav;
